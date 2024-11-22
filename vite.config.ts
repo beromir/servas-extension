d283eb622aedd364e25ee2c74fed201e33b83bdf
Henrik <hreissig@outlook.com>
2024-11-22 19:19:47 +0100

    [TASK] Update Tailwind CSS to v4 beta

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import {resolve} from 'path'
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [svelte(), tailwindcss()],
     base: './',
     build: {
         rollupOptions: {
