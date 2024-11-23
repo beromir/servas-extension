33c5c5c53f197ffc568d73f44dea462f8fb5c92e
Henrik <hreissig@outlook.com>
2024-11-23 17:47:13 +0100

    [TASK] Show a message if settings are missing

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -7,10 +7,12 @@
     import {getCurrentTab} from "./services/tab";
     import {storeLink} from "./services/api";
     import Notification from "./components/Notification.svelte";
+    import {onMount} from "svelte";
+    import {dispatchCustomEvent} from "./utils/util";
 
     const browserAPI = globalThis.chrome || globalThis.browser;
 
-    const options = getOptions();
+    let options: any;
 
     function handleSettingsButtonClick() {
         if (browserAPI.runtime.openOptionsPage) {
@@ -25,6 +27,16 @@
 
         await storeLink(tab);
     }
+
+    onMount(async () => {
+        const result = await getOptions();
+
+        options = result;
+
+        if (!result.servasUrl || !result.apiToken) {
+            dispatchCustomEvent('notify', {message: 'Missing settings', style: 'warning', permanent: true});
+        }
+    })
 </script>
 
 <div class="flex flex-col p-2 h-screen">
@@ -33,11 +45,11 @@
             <CogSixTooth className="fill-gray-500 hover:fill-gray-700"/>
         </button>
 
-        {#await options then data}
-            <a href={data.servasUrl} title="Open Servas">
+        {#if options?.servasUrl}
+            <a href={options.servasUrl} title="Open Servas">
                 <ArrowTopRightOnSquare className="size-4.5 fill-gray-500 hover:fill-gray-700"/>
             </a>
-        {/await}
+        {/if}
     </header>
 
     <main class="flex flex-col justify-between grow">
@@ -45,7 +57,8 @@
 
         </div>
 
-        <Button onclick={handleAddPageButtonClick} color="primary" class="flex! mx-auto">
+        <Button onclick={handleAddPageButtonClick} color="primary" disabled={!options?.servasUrl || !options?.apiToken}
+                class="flex! mx-auto">
             <Plus/>
             Add page
         </Button>
--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -5,11 +5,18 @@
     let message = $state('');
     let style = $state('info');
 
+    let permanent = false;
+
     function handleNotification(event: any) {
         message = event.detail.message ?? '';
         style = event.detail.style ?? 'info';
+        permanent = event.detail.permanent ?? false;
 
         show = true;
+
+        if (permanent) {
+            return;
+        }
 
         setTimeout(() => {
             show = false;
@@ -21,9 +28,11 @@
 
 {#if show}
     <div class={clsx(
-        'absolute inset-x-2 bottom-2 px-2 py-1 text-sm rounded-sm shadow-sm',
-        style === 'success' ? 'bg-green-50 text-green-700 outline outline-green-100' : '',
-        style === 'danger' ? 'bg-red-50 text-red-700 outline outline-red-100' : '',
+        'absolute inset-x-2 bottom-2 px-2 py-1 text-sm rounded-sm shadow-sm outline',
+        style === 'info' ? 'bg-blue-50 text-blue-700 outline-blue-100 dark:bg-blue-950 dark:text-blue-200 outline-blue-900' : '',
+        style === 'success' ? 'bg-green-50 text-green-700 outline-green-100 dark:bg-green-950 dark:text-green-200 outline-green-900' : '',
+        style === 'warning' ? 'bg-yellow-50 text-yellow-700 outline-yellow-100 dark:bg-yellow-950 dark:text-yellow-200 outline-yellow-900' : '',
+        style === 'danger' ? 'bg-red-50 text-red-700 outline-red-100 dark:bg-red-950 dark:text-red-200 outline-red-900' : '',
     )}>
         {message}
     </div>
