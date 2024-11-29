55ca7c78d1f78b643d052fdc7822fb34d5ddeb37
Henrik <hreissig@outlook.com>
2024-11-29 19:12:41 +0100

    [TASK] Render empty state in combobox

--- src/components/Combobox.svelte
+++ src/components/Combobox.svelte
@@ -18,6 +18,7 @@
 
     let showDropdown: boolean = $state(false);
     let searchString: string = $state('');
+    let filteredOptions: any[] = $derived(options.filter(option => option[titleProperty].toLowerCase().includes(searchString.toLowerCase())));
 </script>
 
 <div use:clickOutside={() => showDropdown = false} class="relative">
@@ -30,28 +31,28 @@
     </button>
 
     {#if showDropdown}
-        <ul class="absolute z-10 mt-1 max-h-40 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm dark:bg-gray-900 dark:ring-contrast">
+        <ul class="absolute z-10 mt-1 max-h-40 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm dark:bg-gray-950 dark:ring-contrast">
 
-            {#each options as option}
+            {#each filteredOptions as option}
+                <!-- svelte-ignore a11y_click_events_have_key_events -->
+                <li onclick={() => selectedOptions = toggleArrayValue(selectedOptions, option.id)}
+                    class="relative cursor-default select-none py-1 pl-7 pr-3 text-sm text-gray-900 hover:bg-gray-100 hover:text-black hover:outline-none dark:text-gray-100 dark:hover:bg-gray-900 dark:hover:text-white"
+                    tabindex="-1">
+                    <span class="block truncate"
+                          class:font-semibold={selectedOptions.includes(option.id)}>
+                        {option[titleProperty]}
+                    </span>
 
-                {#if option[titleProperty].toLowerCase().includes(searchString.toLowerCase())}
-                    <!-- svelte-ignore a11y_click_events_have_key_events -->
-                    <li onclick={() => selectedOptions = toggleArrayValue(selectedOptions, option.id)}
-                        class="relative cursor-default select-none py-1 pl-7 pr-3 text-sm text-gray-900 hover:bg-gray-100 hover:text-black hover:outline-none dark:text-gray-100 dark:hover:bg-gray-800 dark:hover:text-white"
-                        tabindex="-1">
-                        <span class="block truncate"
-                              class:font-semibold={selectedOptions.includes(option.id)}>
-                            {option[titleProperty]}
+                    {#if selectedOptions.includes(option.id)}
+                        <span
+                            class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-gray-700 dark:text-gray-200">
+                            <Check/>
                         </span>
+                    {/if}
+                </li>
 
-                        {#if selectedOptions.includes(option.id)}
-                            <span
-                                class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-gray-700 dark:text-gray-200">
-                                <Check/>
-                            </span>
-                        {/if}
-                    </li>
-                {/if}
+            {:else}
+                <li class="py-2 text-sm text-gray-700 text-center dark:text-gray-300">Nothing found</li>
             {/each}
         </ul>
     {/if}
