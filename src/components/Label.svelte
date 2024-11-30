083e90733eac8a8ca5a729cd3791fefeb063443e
Henrik <hreissig@outlook.com>
2024-11-30 12:03:31 +0100

    [TASK] Add and use Inter font

--- src/components/Label.svelte
+++ src/components/Label.svelte
@@ -6,6 +6,6 @@
     let {children, ...props}: { for: string, children: Snippet, props?: HTMLLabelAttributes } = $props();
 </script>
 
-<label {...props} class={clsx(props.class, 'block mb-0.5 text-sm text-gray-600 font-medium')}>
+<label {...props} class={clsx(props.class, 'block mb-1 text-sm text-gray-600 font-medium')}>
     {@render children?.()}
 </label>
