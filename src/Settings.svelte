93f33dbfe22bb5d38d50e1c7c7cc05fa8c7f8079
Henrik <hreissig@outlook.com>
2024-11-30 19:29:56 +0100

    [WIP] Setting view

---
+++ src/Settings.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Input from "./components/Input.svelte";
+</script>
+
+<div class="mx-auto py-12 max-w-xl">
+    <header>
+        <h1 class="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight dark:text-white">
+            Servas Settings
+        </h1>
+    </header>
+
+    <main>
+        <Input id="host" label="Host" placeholder="Host"/>
+
+        <Input id="token" label="API Token" placeholder="API Token"/>
+    </main>
+</div>
--- views/settings.ts
+++ views/settings.ts
@@ -1,32 +1,9 @@
+import { mount } from 'svelte'
 import '../src/app.css'
+import Settings from '../src/Settings.svelte'
 
-// Saves options to browser storage
-function saveOptions() {
-    var servasUrl = document.getElementById('servas-url').value;
-    var apiToken = document.getElementById('api-token').value;
-    browser.storage.sync.set({
-        servasUrl: servasUrl,
-        apiToken: apiToken
-    }, function () {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.innerText = 'Settings saved.';
-        setTimeout(function () {
-            status.innerText = '';
-        }, 1000);
-    });
-}
+const app = mount(Settings, {
+    target: document.getElementById('app')!,
+})
 
-// Restores options stored in browser storage.
-function restoreOptions() {
-    browser.storage.sync.get({
-        servasUrl: '',
-        apiToken: '',
-    }).then(options => {
-        document.getElementById('servas-url').value = options.servasUrl;
-        document.getElementById('api-token').value = options.apiToken;
-    });
-}
-
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('save-options').addEventListener('click', saveOptions);
+export default app
