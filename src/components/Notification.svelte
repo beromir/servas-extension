3a27d5b40df56a2545baa0387d535a82c23299e1
Henrik <hreissig@outlook.com>
2024-12-01 11:33:35 +0100

    [FIX] Fix notification outline color

--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -29,10 +29,10 @@
 {#if show}
     <div class={clsx(
         'absolute inset-x-2 bottom-2 px-2 py-1 text-sm rounded-sm shadow-sm outline',
-        style === 'info' ? 'bg-blue-50 text-blue-700 outline-blue-100 dark:bg-blue-950 dark:text-blue-200 outline-blue-900' : '',
-        style === 'success' ? 'bg-green-50 text-green-700 outline-green-100 dark:bg-green-950 dark:text-green-200 outline-green-900' : '',
-        style === 'warning' ? 'bg-yellow-50 text-yellow-700 outline-yellow-100 dark:bg-yellow-950 dark:text-yellow-200 outline-yellow-900' : '',
-        style === 'danger' ? 'bg-red-50 text-red-700 outline-red-100 dark:bg-red-950 dark:text-red-200 outline-red-900' : '',
+        style === 'info' ? 'bg-blue-50 text-blue-700 outline-blue-200 dark:bg-blue-950 dark:text-blue-200 dark:outline-blue-900' : '',
+        style === 'success' ? 'bg-green-50 text-green-700 outline-green-200 dark:bg-green-950 dark:text-green-200 dark:outline-green-900' : '',
+        style === 'warning' ? 'bg-yellow-50 text-yellow-700 outline-yellow-200 dark:bg-yellow-950 dark:text-yellow-200 dark:outline-yellow-900' : '',
+        style === 'danger' ? 'bg-red-50 text-red-700 outline-red-200 dark:bg-red-950 dark:text-red-200 dark:outline-red-900' : '',
     )}>
         {message}
     </div>
