511d436fe3d726f8cf18a6ee067d015b026a4c51
Henrik <hreissig@outlook.com>
2024-12-01 11:39:56 +0100

    [TASK] Render notification in container

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -28,7 +28,7 @@
     })
 </script>
 
-<div class="mx-auto py-12 max-w-xl">
+<div class="relative mx-auto py-12 max-w-xl">
     <header>
         <h1 class="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight dark:text-white">
             Servas Settings
