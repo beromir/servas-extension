400452151bc0a0251b5affcf7a07a2c60343e9cb
Henrik <hreissig@outlook.com>
2024-12-01 11:37:27 +0100

    [TASK] Add general settings view

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -1,5 +1,31 @@
-<script>
+<script lang="ts">
     import Input from "./components/Input.svelte";
+    import Button from "./components/Button.svelte";
+    import {onMount} from "svelte";
+    import {getOptions, setOption} from "./services/option";
+    import Notification from "./components/Notification.svelte";
+    import {dispatchCustomEvent} from "./utils/util";
+
+    let servasUrl: string = $state('');
+    let apiToken: string = $state('');
+
+    async function handleSaveSettingsButtonClick() {
+        const servasUrlResult = await setOption('servasUrl', servasUrl);
+        const apiTokenResult = await setOption('apiToken', apiToken);
+
+        if (servasUrlResult && apiTokenResult) {
+            dispatchCustomEvent('notify', {message: 'Settings saved', style: 'success'});
+        } else {
+            dispatchCustomEvent('notify', {message: 'There was an error', style: 'danger'});
+        }
+    }
+
+    onMount(async () => {
+        const syncOptionsResult = await getOptions();
+
+        servasUrl = syncOptionsResult?.servasUrl ?? '';
+        apiToken = syncOptionsResult?.apiToken ?? '';
+    })
 </script>
 
 <div class="mx-auto py-12 max-w-xl">
@@ -9,9 +35,31 @@
         </h1>
     </header>
 
-    <main>
-        <Input id="host" label="Host" placeholder="Host"/>
+    <main class="mt-10 space-y-10">
+        <section>
+            <div class="border-b border-gray-200 pb-3">
+                <h2 class="text-base font-semibold text-gray-900">General</h2>
+            </div>
 
-        <Input id="token" label="API Token" placeholder="API Token"/>
+            <div class="mt-6 space-y-5">
+                <Input bind:value={servasUrl} id="host" label="Servas URL" placeholder="https://myservas.com"/>
+
+                <Input bind:value={apiToken} type="password" id="token" label="API Token"/>
+
+                <Button onclick={handleSaveSettingsButtonClick} color="primary" class="mt-2">Save</Button>
+            </div>
+        </section>
+
+        <section>
+            <div class="border-b border-gray-200 pb-3">
+                <h2 class="text-base font-semibold text-gray-900">Appearance</h2>
+            </div>
+
+            <div class="mt-6 space-y-5">
+
+            </div>
+        </section>
     </main>
+
+    <Notification/>
 </div>
